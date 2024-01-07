627ab5e6274611883c8a4124a8b2145fcc32e507
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-01-07 19:25:54 +0100

    Updated page Home.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,13 @@
 import styles from "./Home.module.css";
 
 const Home = (): JSX.Element => {
-  return <div className={styles["home"]}>Home</div>;
+  return (
+    <div className={styles["home"]}>
+      <h2 className={styles["home-title"]}>
+        All our room types are including complementary breakfast
+      </h2>
+    </div>
+  );
 };
 
 export default Home;
