d4addfc0e035ee5c43a4ec775e915a065fe2d4f6
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-01-07 19:05:59 +0100

    Installed optimizeCssModules.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { optimizeCssModules } from "vite-plugin-optimize-css-modules";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteImageOptimizer()],
+  plugins: [react(), ViteImageOptimizer(), optimizeCssModules()],
 });
 
