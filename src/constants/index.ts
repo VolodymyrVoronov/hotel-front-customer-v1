14cb248ecfd2e6480083a18ab6274169ece8ac96
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-01-07 16:25:43 +0100

    Updated constants.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,11 +3,11 @@ export const PATHS = {
   FACILITIES: "/facilities",
   ROOMS: "/rooms",
   CONTACT: "/contact",
-};
+} as const;
 
 export const ROUTES = {
   HOME: PATHS.HOME,
   FACILITIES: PATHS.FACILITIES,
   ROOMS: PATHS.ROOMS,
   CONTACT: PATHS.CONTACT,
-};
+} as const;
