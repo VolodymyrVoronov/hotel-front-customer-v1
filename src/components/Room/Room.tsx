d2c24b02f43bbd6522dc2bdcc6352fcfaeb91432
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-01-11 19:47:49 +0100

    Finished Room.tsx.

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,16 +1,148 @@
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
 import cn from "classnames";
+import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
+import { motion, AnimatePresence } from "framer-motion";
+
+import Button from "../Button/Button";
+import IconButton from "../IconButton/IconButton";
 
 import styles from "./Room.module.css";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 interface IRoomProps extends ComponentProps<"div"> {
+  id: string;
+  roomId: string;
+  roomPrice: number;
+  title: string;
+  photos: {
+    original: string;
+    thumbnail: string;
+  } extends ReactImageGalleryItem
+    ? Array<ReactImageGalleryItem>
+    : never;
+  accordionTitle: string;
+  accordionContentText: string;
+
+  buttonPriceText: string;
+  buttonBookText: string;
+
   className?: string;
 }
 
-const Room = ({ className, ...props }: IRoomProps): JSX.Element => {
+const Room = ({
+  roomId,
+  roomPrice,
+  title,
+  photos,
+  accordionTitle,
+  accordionContentText,
+  buttonPriceText,
+  buttonBookText,
+
+  className,
+
+  ...props
+}: IRoomProps): JSX.Element => {
+  const [toggleBookButton, setToggleBookButton] = useState(false);
+  const [toggleAccordion, setToggleAccordion] = useState(false);
+
+  const onBookButtonClick = (): void => {
+    console.log(`Book room ${roomId}, price ${roomPrice}`);
+  };
+
+  const onBookButtonHover = (): void => {
+    setToggleBookButton(true);
+  };
+
+  const onBookButtonLeave = (): void => {
+    setToggleBookButton(false);
+  };
+
+  const onAccordionButtonClick = (): void => {
+    setToggleAccordion(!toggleAccordion);
+  };
+
   return (
     <div className={cn(styles["room"], className)} {...props}>
-      Room
+      <div className={styles["room-photos"]}>
+        <ImageGallery
+          additionalClass={styles["room-photos-gallery"]}
+          items={photos}
+          showBullets
+          lazyLoad
+          showThumbnails={false}
+          showPlayButton={false}
+          showFullscreenButton={false}
+        />
+      </div>
+      <h2 className={styles["room-title"]}>{title}</h2>
+
+      <div className={styles["room-footer"]}>
+        <div className={styles["room-footer-control"]}>
+          <div className={styles["room-accordion-button-wrapper"]}>
+            <IconButton
+              onClick={onAccordionButtonClick}
+              className={styles["room-accordion-button"]}
+              iconSrc="/icons/plus-icon.svg"
+              iconAlt="Plus icon"
+            />
+            <span className={styles["room-accordion-button-text"]}>
+              {accordionTitle}
+            </span>
+          </div>
+          <Button
+            className={styles["room-book-button"]}
+            onClick={onBookButtonClick}
+            onMouseEnter={onBookButtonHover}
+            onMouseLeave={onBookButtonLeave}
+            hasIcon={false}
+          >
+            <AnimatePresence mode="wait" initial>
+              {toggleBookButton ? (
+                <motion.div
+                  key="book"
+                  className={styles["room-book-text"]}
+                  initial={{ y: 25, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: -25, opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  {buttonBookText}
+                </motion.div>
+              ) : (
+                <motion.div
+                  key="price"
+                  className={styles["room-book-price"]}
+                  initial={{ y: 25, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: -25, opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  {buttonPriceText}
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </Button>
+        </div>
+
+        <div className={styles["room-accordion-content"]}>
+          <AnimatePresence mode="wait">
+            {toggleAccordion ? (
+              <motion.div
+                key="content"
+                className={styles["room-accordion-content-text"]}
+                initial={{ y: -25, height: 0, opacity: 0 }}
+                animate={{ y: 0, height: "auto", opacity: 1 }}
+                exit={{ y: -25, height: 0, opacity: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                <span />
+                {accordionContentText}
+              </motion.div>
+            ) : null}
+          </AnimatePresence>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Rooms/Rooms.tsx
+++ src/pages/Rooms/Rooms.tsx
@@ -1,6 +1,73 @@
 import Heading from "../../components/Heading/Heading";
+import Room from "../../components/Room/Room";
 
 import styles from "./Rooms.module.css";
+
+const rooms = [
+  {
+    id: "1",
+    roomId: "single",
+    roomPrice: 147,
+    title: "Single room",
+    photos: [
+      {
+        original: "/images/rooms/single/single-room-01.jpg",
+        thumbnail: "/images/rooms/single/single-room-01.jpg",
+      },
+      {
+        original: "/images/rooms/single/single-room-02.jpg",
+        thumbnail: "/images/rooms/single/single-room-02.jpg",
+      },
+    ],
+    accordionTitle: "View room details",
+    accordionContentText:
+      "Our rooms are designed to transport you into an environment made for leisure. Take your mind off the day-to-day of home life and find a private paradise for yourself.",
+    buttonPriceText: "$147  Avg/night",
+    buttonBookText: "Book Now",
+  },
+  {
+    id: "2",
+    roomId: "double",
+    roomPrice: 190,
+    title: "Double room",
+    photos: [
+      {
+        original: "/images/rooms/double/double-room-01.jpg",
+        thumbnail: "/images/rooms/double/double-room-01.jpg",
+      },
+      {
+        original: "/images/rooms/double/double-room-02.jpg",
+        thumbnail: "/images/rooms/double/double-room-02.jpg",
+      },
+    ],
+    accordionTitle: "View room details",
+    accordionContentText:
+      "Our rooms are designed to transport you into an environment made for leisure. Take your mind off the day-to-day of home life and find a private paradise for yourself.",
+    buttonPriceText: "$190  Avg/night",
+    buttonBookText: "Book Now",
+  },
+  {
+    id: "3",
+    roomId: "twin",
+    roomPrice: 255,
+    title: "Twin room",
+    photos: [
+      {
+        original: "/images/rooms/twin/twin-room-01.jpg",
+        thumbnail: "/images/rooms/twin/twin-room-01.jpg",
+      },
+      {
+        original: "/images/rooms/twin/twin-room-02.jpg",
+        thumbnail: "/images/rooms/twin/twin-room-02.jpg",
+      },
+    ],
+    accordionTitle: "View room details",
+    accordionContentText:
+      "Our rooms are designed to transport you into an environment made for leisure. Take your mind off the day-to-day of home life and find a private paradise for yourself.",
+    buttonPriceText: "$255  Avg/night",
+    buttonBookText: "Book Now",
+  },
+];
 
 const Rooms = (): JSX.Element => {
   return (
@@ -16,6 +83,24 @@ const Rooms = (): JSX.Element => {
         complemented by the rich tones of nature’s palette as visible from our
         rooms’ sea-view windows and terraces.
       </span>
+
+      <div className={styles["rooms-items"]}>
+        {rooms.map((room) => (
+          <Room
+            className={styles["rooms-item"]}
+            key={room.id}
+            id={room.id}
+            roomId={room.roomId}
+            roomPrice={room.roomPrice}
+            title={room.title}
+            photos={room.photos}
+            accordionTitle={room.accordionTitle}
+            accordionContentText={room.accordionContentText}
+            buttonPriceText={room.buttonPriceText}
+            buttonBookText={room.buttonBookText}
+          />
+        ))}
+      </div>
     </div>
   );
 };
